ca82878fc56169b2bbd42986612a8b7b528166a7
y-du <42994541+y-du@users.noreply.github.com>
2020-08-21 16:01:40 +0200

    init

---
+++ components/protocol-adapters/deployments/component.js
@@ -0,0 +1,49 @@
+/*
+    Copyright 2020 InfAI (CC SES)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+export { Deployments }
+
+import { List } from './list/list.min.js'
+/*import { Form } from './form/form.min.js'
+import { Details } from "./details/details.min.js";*/
+
+
+class Deployments {
+    static api = '/api/deployment-manager/deployments';
+
+    constructor(ctr) {
+        this.list = new List(ctr);
+        /*this.form = new Form(ctr);
+        this.details = new Details(ctr);*/
+    }
+
+    draw(sp, {pa}) {
+        if (!sp && !pa) {
+            return this.list.draw();
+        }
+        if (sp && pa && sp.has('action') && sp.get('action') === 'details') {
+            return this.details.draw(pa);
+        }
+        if (sp && !pa && sp.has('action') && sp.get('action') === 'new') {
+            return this.form.draw();
+        }
+        if (sp && pa && sp.has('action') && sp.get('action') === 'edit') {
+            return this.form.drawEdit(pa);
+        }
+    }
+
+
+}
---
+++ components/protocol-adapters/deployments/list/list.js
@@ -0,0 +1,71 @@
+/*
+    Copyright 2020 InfAI (CC SES)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+export { List }
+
+class List {
+
+    constructor(ctr) {
+        this.container = ctr;
+    }
+
+    draw() {
+        fetch(active_cmp.constructor.api + '?type=protocol-adapter')
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw 'Error retrieving deployments - ' + response.status;
+            })
+            .then((data) => {
+                fetch('/components/protocol-adapters/deployments/list/template.html')
+                    .then((response) => response.text())
+                    .then((template) => {
+                        let items = [];
+                        for (let [key, value] of Object.entries(data)) {
+                            items.push(
+                                {
+                                    d_id: key,
+                                    pa_id: value['labels']['lopco-id'],
+                                    image: value['image']['name'],
+                                    hash: value['image']['hash']
+                                }
+                            )
+                        }
+                        this.container.innerHTML = Mustache.render(template, {pa_items: items});
+                    });
+            })
+            .catch((err) => {
+                this.container.innerHTML = err
+            });
+    }
+
+    delete(id) {
+        if (confirm('Delete ' + id + '?')) {
+            fetch(active_cmp.constructor.api + '/' + id, {method: 'DELETE'})
+                .then((response) => {
+                    if (response.ok) {
+                        window.open('/protocol-adapters/deployments','_self');
+                    } else {
+                        throw response.status;
+                    }
+                })
+                .catch((err) => {
+                    alert('Error deleting ' + id + ' - ' + err);
+                })
+        }
+    }
+}
